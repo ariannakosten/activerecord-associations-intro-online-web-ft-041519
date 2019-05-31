ee8386c74d0deb81a40af5f4eecab3ea20150a02
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-31 07:24:45 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ActiveRecord::Base
-  has_many :songs
-  has_many :genres, 
-  through: :songs
+  has_many :songs  #artist has many songs
+  has_many :genres, #artist has many genres...
+  through: :songs  #...through songs class
 end
