24690a8a3c7fd0549c431aa017843bf9dec07af2
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-31 07:17:49 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,5 +1,5 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
-class CreateSongs < ActiveRecord::Migration[4.2]
+  
   def change
     create_table :songs do |t|
       t.string :name 
@@ -8,4 +8,4 @@ class CreateSongs < ActiveRecord::Migration[4.2]
     end
   end
 end
-end
+
